bc8cf3e9130dbbd165dd28adb42030968699ef29
Bennett Gould <bgould@protonmail.com>
2020-10-09 03:06:21 -0400

    added additional file structure and comments or teammates

---
+++ htmlBuilder.js
@@ -0,0 +1,6 @@
+//Here is where we can use jQuery to dynamically insert addiontal HTML pages into the primary page
+//using $('.class').load('htmlFileName.html');
+//this will allow for much cleaner front end development without useing a templater like handlebars
+
+//For example, this is where we will inject the modalExample code into index.html without having a crazy tree
+//in that file
