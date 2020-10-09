1a7210fa6912ec8c695887f05caa8fbd9c779a3f
Bennett Gould <bgould@protonmail.com>
2020-10-09 02:27:04 -0400

    refactored firebase js for HTML readability

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyBW1m6pnj5bjwyJJPfo3UKTolBGy2V6gYA",
+        authDomain: "seotracker-3e0ec.firebaseapp.com",
+        databaseURL: "https://seotracker-3e0ec.firebaseio.com",
+        projectId: "seotracker-3e0ec",
+        storageBucket: "seotracker-3e0ec.appspot.com",
+        messagingSenderId: "807917864943",
+        appId: "1:807917864943:web:52562e07a2f714fdb3d81a",
+        measurementId: "G-0LJ02T531T"
+    };
+// Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+   
