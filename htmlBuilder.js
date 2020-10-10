8b2c0e7e6612b4c94f8657ed0d0405934457d232
Bennett Gould <bgould@protonmail.com>
2020-10-10 01:09:15 -0400

    working on templating structure for adding modals dynamically

--- htmlBuilder.js
+++ htmlBuilder.js
@@ -3,12 +3,27 @@
 //this will allow for much cleaner front end development without useing a templater like handlebars
 // $('.modalContent').load('modalExample.html body');
 // $('#modalTrigger').attr('href','#modal1')
-$(function(){
-    // $('.modalContent').append($('#template').html());
+
+// $(function(){
+//     // $('.modalContent').append($('#modal1').html());
+//     $('.modalContent').load('modalExample.html');
     
-    $('.modal').modal();
-    $('#modalTrigger').attr('href','#modal1').addClass('modal-trigger');
-    // $('#modal1').modal('open');
+//     $('.modal').modal();
+//     $('#modalTrigger').attr('href','.modal1').addClass('modal-trigger');
+//     // $('#modal1').modal('open');
+// });
+
+$(function(){
+    const supportsTemplate = function(){
+        return 'content' in document.createElement('template');
+    };
+    document.addEventListener('DOMContentLoaded', ()=> {
+        if(supportsTemplate()){
+            let temp = document.getElementById('firstModal');
+            let content2 = temp.content;
+            $('#modalContent').append(content2);
+            console.log("template loaded");
+        }
+    });
+
 });
-
-
