1f6153c5cf6f47c6e0e24a79de4a0897ce65f7cd
Bennett Gould <bgould@protonmail.com>
2020-10-09 23:32:36 -0400

    wahoo got a modal working with Materialize front end!

--- htmlBuilder.js
+++ htmlBuilder.js
@@ -1,10 +1,14 @@
 //Here is where we can use jQuery to dynamically insert addiontal HTML pages into the primary page
 //using $('.class').load('htmlFileName.html');
 //this will allow for much cleaner front end development without useing a templater like handlebars
+// $('.modalContent').load('modalExample.html body');
+// $('#modalTrigger').attr('href','#modal1')
+$(function(){
+    // $('.modalContent').append($('#template').html());
+    
+    $('.modal').modal();
+    $('#modalTrigger').attr('href','#modal1').addClass('modal-trigger');
+    // $('#modal1').modal('open');
+});
 
-//For example, this is where we will inject the modalExample code into index.html without having a crazy tree
-//in that file
-// $('.launchModal').on('click',function(){
-//     $('#exampleModal1').foundation('reveal','open');
-// });
-$(document).foundation();
+
