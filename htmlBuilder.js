4a15d756b9a612f611abe47a2d1bf8f2c3d64762
Bennett Gould <bgould@protonmail.com>
2020-10-10 05:44:44 -0400

    added modal for creating an account!

--- htmlBuilder.js
+++ htmlBuilder.js
@@ -3,22 +3,15 @@
 //this will allow for much cleaner front end development without useing a templater like handlebars
 // $('.modalContent').load('modalExample.html body');
 // $('#modalTrigger').attr('href','#modal1')
-var modalz = 
-`<div id="modal1" class="modal">
-    <div class="modal-content">
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-    </div>
-    <div class="modal-footer">
-        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
-</div>`;
-
-
 $(function(){
     testForMobile();
     $('.modalContent').html(modalz);
     $('#modalTrigger').attr('href','#modal1').addClass('modal-trigger');
+    $('.createAnAccount').html(createAcountContent);
+    $('#createAccountTrig').attr('href','#createAccountModal').addClass('modal-trigger');
+  
+    
+    //initialize Materialize content last so that it is available
     $('.modal').modal(); //needed in order to initialize Materialize modals
     $('.sidenav').sidenav(); //needed in order to initialize side bar for mobile menu
 
@@ -30,3 +23,55 @@ function testForMobile(){
         $('.brand-logo').removeAttr('id'); //centers the logo on mobile devices -- remember to refresh in inspector
     }
 }
+var modalz = 
+`<div id="modal1" class="modal">
+    <div class="modal-content">
+        <h4>Modal Header</h4>
+        <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+</div>`;
+var createAcountContent =
+`<div id="createAccountModal" class="modal">
+    <div class="row modal-content">
+        <div class="col s12 m10 l8">
+            <div class="card-panel ">
+                <div class="row">
+                    <form class="col s12 m10 l8">
+                        <div class="row">
+                            <div class="input-field col s8">
+                                <i class="mdi-communication-email prefix"></i>
+                                <input id="email" type="email" class="validate">
+                                <label for="email">Your email</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s8">
+                                <i class="mdi-action-lock-outline prefix"></i>
+                                <input id="password" type="password" class="validate">
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s8">
+                                <i class="mdi-action-lock-outline prefix"></i>
+                                <input id="password" type="password" class="validate">
+                                <label for="password">Confirm Password</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <a href="#" class="btn waves-effect waves-light col "><h6>Create Account</h6></a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+</div>`;
