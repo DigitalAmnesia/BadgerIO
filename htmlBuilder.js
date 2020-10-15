d6c44968544db1f42caf0d8fa9261f1bb0ed1c8d
Habby Olusesi <habbyolu@gmail.com>
2020-10-15 18:44:01 -0400

    repush of code

--- htmlBuilder.js
+++ htmlBuilder.js
@@ -194,7 +194,7 @@ var aboutContent =
         <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content">Details</a></div></li>
         <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content">Details</a></div></li>
         <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content">Details</a></div></li>
-        <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">Details</a></div></li>
+        <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content">Details</a></div></li>
     </ul>
 </div>`;
 /**
